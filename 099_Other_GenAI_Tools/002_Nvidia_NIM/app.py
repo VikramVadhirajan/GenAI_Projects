5269e19f300637350d2710a76c94cdd68448241c
Vikram Vadhirajan <viva.1205@gmail.com>
2026-04-29 15:57:20 +0530

    NVIDIA NIM Project

---
+++ 099_Other_GenAI_Tools/002_Nvidia_NIM/app.py
@@ -0,0 +1,27 @@
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()  
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = os.getenv("NVIDIA_API_KEY")
+)
+
+completion = client.chat.completions.create(
+  model="meta/llama-3.1-8b-instruct",
+  messages=[{"role":"user","content":"Hi What is your model name give me in one line."}],
+  temperature=1,
+  top_p=1,
+  max_tokens=4096,
+  stream=True
+)
+
+for chunk in completion:
+  if not getattr(chunk, "choices", None):
+    continue
+  reasoning = getattr(chunk.choices[0].delta, "reasoning_content", None)
+  if reasoning:
+    print(reasoning, end="")
+  if chunk.choices and chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
+
---
+++ 099_Other_GenAI_Tools/002_Nvidia_NIM/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from 002-nvidia-nim!")
+
+
+if __name__ == "__main__":
+    main()
